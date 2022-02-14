fee17e1605405e4b7f06e1a5cb4e34ab522e72c0
admin <nikfallah.mahdi@gmail.com>
2022-02-14 19:38:17 +0200

    initial

---
+++ Assets/Logitech VR Ink/Models/LogitechVRInkModels/SimpleModel/MakeMark.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class MakeMark : MonoBehaviour
+{
+
+    public Material OnMaterial;
+    public Material OffMaterial;
+    public GameObject Marker;
+    private GameObject parentObj;
+    public SteamVR_Action_Boolean MarkButtonPressed;
+    public SteamVR_Input_Sources handType;
+    private GameObject childObject;
+    private void Start()
+    {
+        MarkButtonPressed.AddOnStateDownListener(TriggerDown, handType);
+        MarkButtonPressed.AddOnStateUpListener(TriggerUp, handType);
+        transform.GetComponent<Renderer>().material = OffMaterial;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "MarkableData")
+        {
+            transform.GetComponent<Renderer>().material = OnMaterial;
+            parentObj = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "MarkableData")
+        {
+            parentObj = null;
+            transform.GetComponent<Renderer>().material = OffMaterial;
+        }
+
+    }
+
+    public void TriggerUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+
+    }
+
+    public void TriggerDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        if (parentObj)
+        {
+            if (childObject == null)
+            {
+                childObject = Instantiate(Marker);
+                
+                childObject.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+            }
+            childObject.transform.position = transform.position;
+            childObject.transform.parent = parentObj.transform;
+            childObject.tag = "Marker";
+            Rigidbody corb = childObject.GetComponent<Rigidbody>();
+        }
+
+
+    }
+
+
+}
---
+++ Assets/ResetScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetScene : MonoBehaviour
+{
+    public Transform ResetPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.position = Camera.main.transform.position + (Camera.main.transform.forward * 0.5f);
+        }
+
+
+    }
+}
